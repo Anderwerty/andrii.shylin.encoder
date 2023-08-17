acabb89cc37b4898b039dedf7873c112f5c2fa8c
Andrii <andrii.shylin@gmailcom>
2023-08-17 20:58:17 +0300

    add hello

--- src/main/java/ua/javarush/encoder/Application.java
+++ src/main/java/ua/javarush/encoder/Application.java
@@ -1,7 +1,8 @@
 package ua.javarush.encoder;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String args []) {
+        System.out.println("Hello");
 
     }
 }
